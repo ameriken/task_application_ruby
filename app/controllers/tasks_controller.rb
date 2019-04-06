9f6cc2fedca6b62130df7a78dfe76f7e87c1d9fd
AMEMIYA_KENJI_MAC <hitohahito@hotmail.co.jp>
2019-04-07 02:49:26 +0900

    [Updated]flashメッセージの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,8 @@ class TasksController < ApplicationController
   def create
     task = Task.new (task_params)
     task.save!
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+    flash[:notice] = "タスク「#{task.name}」を登録しました。"
+    redirect_to tasks_url
 
   end
 

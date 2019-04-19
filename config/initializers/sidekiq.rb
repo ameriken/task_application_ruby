2c77e8dd3c717ede427207d316dd069367b5fb95
AMEMIYA_KENJI_MAC <hitohahito@hotmail.co.jp>
2019-04-20 03:10:19 +0900

    [Updated]Redis関連の調整

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://redis:6379', namespace: "sidekiq" }
 end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://redis:6379', namespace: "sidekiq" }
+end
